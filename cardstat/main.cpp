139c17e5daf1ff0373d82c7628075a5edefef5cd
Fouad <fouadre2317@gmail.com>
2025-12-02 22:55:53 +0100

    ho aggiunto la visualizazione grafica delle carte del mazza da 40 manca sistemare gli spazzi e il mazzo da 52

--- cardstat/main.cpp
+++ cardstat/main.cpp
@@ -8,28 +8,21 @@
 using namespace std;
 
 
-
 void scelte(int &scelta, int &nEstrazioni);
 void random(int &seme,int &valore, int &scelta);
 void confronti (int &scelta, int &nEstrazioni,int &coppia);
+void disegna_carte(int scelta ,int valore ,int valorePrecedente ,int seme ,int semePrecedente);
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-    cout << R"(
-                ┌─────────┐   ┌─────────┐   ┌─────────┐   ┌─────────┐
-                │4        │   │A        │   │J        │   │10       │
-                │         │   │         │   │         │   │         │
-                │    ♦    │   │    ♣    │   │    ♠    │   │    ♥    │
-                │         │   │         │   │         │   │         │
-                │        4│   │        A│   │        J│   │       10│
-                └─────────┘   └─────────┘   └─────────┘   └─────────┘
-            )" << endl;
+    srand(time(NULL));
     int scelta = 0;
     int nEstrazioni = 0;
+    int coppia = 00;
     scelte(scelta, nEstrazioni);
-
+    confronti (scelta ,nEstrazioni ,coppia);
     return 0;
 }
 
@@ -65,7 +58,6 @@ void random(int &seme,int &valore, int &scelta)
 {
 
     if (scelta == 1) {
-        srand(time(NULL));
         valore = rand () %10+1;
         seme = rand () %4+1;
     } else if (scelta == 2) {
@@ -84,9 +76,411 @@ void confronti (int &scelta, int &nEstrazioni,int &coppia)
 
     for (int i = 0; i<nEstrazioni; i++) {
         random (seme, valore, scelta); // 3 e 4 (valori) sono neri
+        disegna_carte(scelta ,valore ,valorePrecedente ,seme ,semePrecedente);
         if (semePrecedente != seme && valorePrecedente == valore) {
             coppia++;
             }
         }
 
 }
+
+void disegna_carte(int scelta ,int valore ,int valorePrecedente ,int seme ,int semePrecedente)
+{
+    if (scelta == 1){
+            if (valore >=8){
+                switch (valorePrecedente){
+                case 8:
+                    switch (semePrecedente){
+                        case 1:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♥    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                        case 2:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♦    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                        case 3:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♣    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                        case 4:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♠    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                    }
+                break;
+                case 9:
+                    switch (semePrecedente){
+                        case 1:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♥    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                        case 2:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♦    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                        case 3:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♣    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                        case 4:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♠    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                    }
+                break;
+                case 10:
+                    switch (semePrecedente){
+                        case 1:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♥    │
+│         │
+│        K│
+└─────────┘
+                )";
+                        break;
+                        case 2:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♦    │
+│         │
+│        K│
+└─────────┘
+                )" << endl;
+                        break;
+                        case 3:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♣    │
+│         │
+│        K│
+└─────────┘
+                )";
+                        break;
+                        case 4:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♠    │
+│         │
+│        K│
+└─────────┘
+                )";
+                        break;
+                    }
+                }
+                switch (semePrecedente){
+                    case 1:
+                        cout << R"(
+┌─────────┐
+|)"<<valorePrecedente<< R"(        │
+│         │
+│    ♥    │
+│         │
+│        )"<<valorePrecedente<< R"(│
+└─────────┘
+                )";
+                    break;
+                    case 2:
+                        cout << R"(
+┌─────────┐
+|)"<<valorePrecedente<< R"(        │
+│         │
+│    ♦    │
+│         │
+│        )"<<valorePrecedente<< R"(│
+└─────────┘
+                )";
+                    break;
+                    case 3:
+                        cout << R"(
+┌─────────┐
+|)"<<valorePrecedente<< R"(        │
+│         │
+│    ♣    │
+│         │
+│        )"<<valorePrecedente<< R"(│
+└─────────┘
+                )";
+                    break;
+                    case 4:
+                        cout << R"(
+┌─────────┐
+|)"<<valorePrecedente<< R"(        │
+│         │
+│    ♠    │
+│         │
+│        )"<<valorePrecedente<< R"(│
+└─────────┘
+                )";
+                    break;
+                }
+                switch (valore){
+                case 8:
+                    switch (seme){
+                        case 1:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♥    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                        case 2:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♦    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                        case 3:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♣    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                        case 4:
+                            cout << R"(
+┌─────────┐
+│J        │
+│         │
+│    ♠    │
+│         │
+│        J│
+└─────────┘
+                )";
+                        break;
+                    }
+                break;
+                case 9:
+                    switch (seme){
+                        case 1:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♥    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                        case 2:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♦    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                        case 3:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♣    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                        case 4:
+                            cout << R"(
+┌─────────┐
+│Q        │
+│         │
+│    ♠    │
+│         │
+│        Q│
+└─────────┘
+                )";
+                        break;
+                    }
+                    break;
+                    case 10:
+                        switch (seme){
+                        case 1:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♥    │
+│         │
+│        K│
+└─────────┘
+                )";
+                        break;
+                        case 2:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♦    │
+│         │
+│        K│
+└─────────┘
+                )";
+                        break;
+                        case 3:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♣    │
+│         │
+│        K│
+└─────────┘
+                )";
+                        break;
+                        case 4:
+                            cout << R"(
+┌─────────┐
+│K        │
+│         │
+│    ♠    │
+│         │
+│        K│
+└─────────┘
+                )";
+                        break;
+                    }
+                }
+            }
+            cout << R"(
+
+
+
+
+
+
+
+                )";
+                switch (seme){
+                    case 1:
+                        cout << R"(
+┌─────────┐
+|)"<<valore<< R"(        │
+│         │
+│    ♥    │
+│         │
+│        )"<<valore<< R"(│
+└─────────┘
+                )";
+                    break;
+                    case 2:
+                        cout << R"(
+┌─────────┐
+|)"<<valore<< R"(        │
+│         │
+│    ♦    │
+│         │
+│        )"<<valore<< R"(│
+└─────────┘
+                )";
+                    break;
+                    case 3:
+                        cout << R"(
+┌─────────┐
+|)"<<valore<< R"(        │
+│         │
+│    ♣    │
+│         │
+│        )"<<valore<< R"(│
+└─────────┘
+                )";
+                    break;
+                    case 4:
+                        cout << R"(
+┌─────────┐
+|)"<<valore<< R"(        │
+│         │
+│    ♠    │
+│         │
+│        )"<<valore<< R"(│
+└─────────┘
+                )";
+                    break;
+            }
+        }else{
+    }
+}
+
